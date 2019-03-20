142e834c6eb759a29affe0addea309ccc5d77275
James York <kroysemaj@gmail.com>
2019-03-19 22:58:47 -0400

    first commit, where we landed at the end of the night at Software Guild.

--- javascript/setup.js
+++ javascript/setup.js
@@ -1,3 +1,8 @@
-function compliment(){
-  return "You look nice today"
+function convertSingleMinutes(time) {
+  const results = ["0000", "Y000", "YY00"];
+  return results[isDivisibleBy5(time)];
+}
+
+function isDivisibleBy5(mins) {
+  return mins % 5;
 }
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,9 +1,21 @@
-describe('Setting up tests with Jasmine', function(){
-  it('should be true', function() {
-    expect(true).toBe(true);
+describe("Berlin Clock", () => {
+  it("0", () => {
+    expect(convertSingleMinutes(0)).toBe("0000");
+  });
+  it("1", () => {
+    expect(convertSingleMinutes(1)).toBe("Y000");
+  });
+  it("5", () => {
+    expect(convertSingleMinutes(5)).toBe("0000");
+  });
+  it("10", () => {
+    expect(convertSingleMinutes(10)).toBe("0000");
+  });
+  it("6", () => {
+    expect(convertSingleMinutes(6)).toBe("Y000");
   });
 
-  it('should see the source file', function() {
-    expect(compliment()).toBe('You look nice today');
+  it("2", () => {
+    expect(convertSingleMinutes(2)).toBe("YY00");
   });
 });
